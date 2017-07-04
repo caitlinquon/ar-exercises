1ae9f42176ebdcc71440747ffe1b51d4864a3b42
caitlinquon <caitlin_quon@hotmail.com>
2017-07-04 23:34:42 +0000

    ar exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,34 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+@store1.employees.create(
+  first_name: "Caitlin", 
+  last_name: "Quon", 
+  hourly_rate: 60)
+@store1.employees.create(
+  first_name: "Emma", 
+  last_name: "Quon", 
+  hourly_rate: 70)
+
+@store1.employees.create(
+  first_name: "Leo", 
+  last_name: "Quon", 
+  hourly_rate: 100)
+
+@store2.employees.create(
+  first_name: "Sophie", 
+  last_name: "Quon", 
+  hourly_rate: 180)
+
+@store2.employees.create(
+  first_name: "Meghan", 
+  last_name: "Quon", 
+  hourly_rate: 40)
+
+@store2.employees.create(
+  first_name: "Liz", 
+  last_name: "Quon", 
+  hourly_rate: 65)
